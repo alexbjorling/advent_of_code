f6db1393e4e2c48acfe42e4686026f4d10257f26
Alexander Björling <alexanbj@axis.com>
2023-12-07 11:57:30 +0100

    day 5
    Change-Id: Ie6bfcc675ff401426e8e6f8ac975f36cac398690

--- 2023/05/part2.py
+++ 2023/05/part2.py
@@ -1,98 +1,66 @@
+"""
+Now ranges are represented by simple tuples (min, max), with min and max inclusive.
+"""
+
 import re
 
-
-class Range(object):
-	"""
-	Trivial helper class to represent a continuous range of integers
-	"""
-	def __init__(self, start, num):
-		self.start = start
-		self.num = num
-		self.max = start + num - 1
-
-	def overlap(self, other):
-		return (self.start <= other.max and self.max >= other.start)
-
-	def split(self, pos):
-		# split range so that pos becomes a new first element
-		ret = [Range(self.start, pos - self.start), Range(pos, self.num - (pos - self.start))]
-		assert ret[0].num > 0 and ret[1].num > 0, 'Couldn''t split range at %d' % pos
-		return ret
-
-	def partial_overlap(self, other):
-		# return the first value at which our range should be split
-		# to not overlap with <other>, or None if there's no overlap.
-		if (self.start <= other.max) and (self.max > other.max):
-			# our range crosses the upper bound of other
-			return other.max + 1
-		elif (self.max >= other.start) and (self.start < other.start):
-			# our range crosses the lower bound of other
-			return other.start
-		else:
-			return None
-
-	def contains(self, num):
-		return (num >= self.start) and (num <= self.max)
-
-	def __str__(self):
-		return '[%d, %d]' % (self.start, self.max)
-
-
+# helper class for the mapping
 class Map(object):
-	"""
-	Helper class which holds a map and can operate on lists of Range objects
-	"""
-	def __init__(self, lines=[]):
-		self.lines = lines
+    def __init__(self, lines):
+        self.lines = lines
 
-	def map(self, r):
-		# takes a single range and returns a list of mapped ranges
-		###################################################################################################################################################
-		####### problem here is that we return in the lines loop and so only treat the first line of each map     ##########################
-		##################################################################################################################
-		mapped_ranges = []  # a list of ranges that are done and shouldn't be operated on
-		for line in self.lines:
-			dest_start, src_start, range_length = line
-			src_range = Range(src_start, range_length)
-			intersection = r.partial_overlap(src_range)
-			if intersection is None:
-				if src_range.contains(r.start):
-					# whole range is mapped
-					return [Range(dest_start + (r.start - src_start), r.num)]
-				else:
-					# none of the range is mapped
-					return [r,]
-			else:
-				ret = []
-				for r_ in r.split(intersection):
-					ret += self.map(r_)
-				return ret
+    def map_ranges(self, ranges):
+        """
+        Map a list of source ranges to a list of destination ranges
+        """
+        line_input = ranges
+        mapped = []
+        for line in self.lines:
+            dest_start, src_start, range_length = line
+            src_end = src_start + range_length - 1
+            shift = dest_start - src_start
+            remaining_input = []
+            for r in line_input:
+                if (r[1] < src_start) or (r[0] > src_end):  # this range is not mapped
+                    remaining_input.append(r)
+                elif (r[0] >= src_start) and (r[1] <= src_end):  # this whole range is mapped
+                    mapped.append((r[0] + shift, r[1] + shift))
+                elif (r[0] < src_start) and (r[1] >= src_start) and (r[1] <= src_end):  # top of this range is mapped but not bottom
+                    remaining_input.append((r[0], src_start - 1))
+                    overlap = r[1] - src_start + 1
+                    mapped.append((dest_start, dest_start + overlap - 1))
+                elif (r[0] >= src_start) and (r[0] <= src_end) and (r[1] > src_end):  # bottom of this range is mapped but not top
+                    overshoot = r[1] - src_end
+                    mapped.append((r[0] + shift, src_end + shift))
+                    remaining_input.append((src_end + 1, src_end + overshoot))
+                elif (r[0] < src_start) and (r[1] > src_end):  # middle of this range is mapped but not ends
+                    before = src_start - r[0]
+                    after = r[1] - src_end
+                    mapped.append((dest_start, dest_start + range_length - 1))
+                    remaining_input.append((src_start - before, src_start - 1))
+                    remaining_input.append((src_end + 1, src_end + after))
+                else:
+                    raise Exception('Should not get here (%s, (%d,%d))!' % (r, src_start, src_end))
+            line_input = remaining_input
+        return remaining_input + mapped
 
 # text parsing - assuming maps are in order
 seeds = []
 maps = []
-with open('ex.txt', 'r') as fp:
-	for line in fp:
-		if 'seeds:' in line:
-			seeds_ = list(map(int, re.findall('([0-9]+)', line)))
-			seeds = [Range(seeds_[i], seeds_[i+1]) for i in range(0, len(seeds_), 2)]
-		elif 'map:' in line:
-			maps.append(Map(lines=[]))
-		else:
-			numbers = list(map(int, re.findall('([0-9]+)', line)))
-			if len(numbers):
-				maps[-1].lines.append(numbers)
+with open('input.txt', 'r') as fp:
+    for line in fp:
+        if 'seeds:' in line:
+            seeds_ = list(map(int, re.findall('([0-9]+)', line)))
+            seeds = [(seeds_[i], seeds_[i] + seeds_[i+1] - 1) for i in range(0, len(seeds_), 2)]
+        elif 'map:' in line:
+            maps.append(Map(lines=[]))
+        else:
+            numbers = list(map(int, re.findall('([0-9]+)', line)))
+            if len(numbers):
+                maps[-1].lines.append(numbers)
 
-minima = []
-for s in seeds:
-	seeds_in = [s,]
-	print('*** seed:', str(s))
-	for m in maps:
-		seeds_out = []
-		for s_ in seeds_in:
-			seeds_out += m.map(s_)
-		print('step: ', [str(s_) for s_ in seeds_out])
-		seeds_in = seeds_out
-	minima.append(min([r.start for r in seeds_out]))
+for m in maps:
+    seeds = m.map_ranges(seeds)
 
-print('*** ', min(minima), ' ***')
+result = min([r[0] for r in seeds])
+assert result == 78775051
