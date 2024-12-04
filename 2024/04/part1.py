f43f850c66dc23ac06d268acf55c91cf40d3c196
Alexander Björling <alexanbj@axis.com>
2024-12-04 21:18:17 +0100

    2024/04 tidy

--- 2024/04/part1.py
+++ 2024/04/part1.py
@@ -1,22 +1,19 @@
 import re
-import numpy as np
 
-data = []
 with open("input.txt", "r") as fp:
-    for line in fp:
-        data.append(line.strip())
+    data = fp.read()
 
-tot = 0
-length = len(data[0].strip())  # without newline
-data = ' '.join(data)          # space instead of newline, to match . in regex
+line_length = len(data.split()[0])  # without newline
+data = data.replace("\n", " ")      # newline doesn't match . in regex
 
 patterns = [
     "XMAS",  # horizontal
-    "X(?=.{%d}M.{%d}A.{%d}S)" % ((length,)*3),  # vertical
-    "X(?=.{%d}M.{%d}A.{%d}S)" % ((length + 1,)*3),  # diagonal 1
-    "X(?=.{%d}M.{%d}A.{%d}S)" % ((length - 1,)*3),  # diagonal 2
+    "X(?=.{%d}M.{%d}A.{%d}S)" % ((line_length,)*3),  # vertical
+    "X(?=.{%d}M.{%d}A.{%d}S)" % ((line_length + 1,)*3),  # diagonal 1
+    "X(?=.{%d}M.{%d}A.{%d}S)" % ((line_length - 1,)*3),  # diagonal 2
 ]
 
+tot = 0
 for p in patterns:
     tot += len(re.findall(p, data))
     tot += len(re.findall(p, data[::-1]))
--- 2024/04/part2.py
+++ 2024/04/part2.py
@@ -1,23 +1,20 @@
 import re
-import numpy as np
 
-data = []
 with open("input.txt", "r") as fp:
-    for line in fp:
-        data.append(line.strip())
+    data = fp.read()
 
-tot = 0
-length = len(data[0].strip())  # without newline
-data = ' '.join(data)          # space instead of newline, to match . in regex
+line_length = len(data.split()[0])  # without newline
+data = data.replace("\n", " ")      # newline doesn't match . in regex
 
 patterns = [
     # four rotations of the cross
-    "M(?=.M.{%d}A.{%d}S.S)" % ((length-1,)*2),
-    "M(?=.S.{%d}A.{%d}M.S)" % ((length-1,)*2),
-    "S(?=.S.{%d}A.{%d}M.M)" % ((length-1,)*2),
-    "S(?=.M.{%d}A.{%d}S.M)" % ((length-1,)*2),
+    "M(?=.M.{%d}A.{%d}S.S)" % ((line_length-1,)*2),
+    "M(?=.S.{%d}A.{%d}M.S)" % ((line_length-1,)*2),
+    "S(?=.S.{%d}A.{%d}M.M)" % ((line_length-1,)*2),
+    "S(?=.M.{%d}A.{%d}S.M)" % ((line_length-1,)*2),
 ]
 
+tot = 0
 for p in patterns:
     tot += len(re.findall(p, data))
 
