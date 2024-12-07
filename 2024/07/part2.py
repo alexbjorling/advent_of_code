0d61496630dd2c0a87b4c2dd1f268db4e8b41010
Alexander Björling <alexanbj@axis.com>
2024-12-07 07:03:27 +0100

    2024/07 parallel

--- 2024/07/part2.py
+++ 2024/07/part2.py
@@ -1,13 +1,17 @@
 import re
 import itertools
+import multiprocessing
 
 eqs = []
 with open("input.txt", "r") as fp:
     for line in fp:
         eqs.append(tuple(map(int, re.findall("\d+", line))))
 
-tot = 0
-for eq in eqs:
+def check_eqn(eq):
+    """
+    Returns the test result if there is a combination of operators
+    that makes the equation whole. Otherwise zero.
+    """
     for operators in itertools.product('+*|', repeat=len(eq)-2):
         res = eq[1]
         for i in range(len(eq) - 2):
@@ -17,8 +21,13 @@ for eq in eqs:
                 res *= eq[i + 2]
             else:
                 res = int(str(res) + str(eq[i + 2]))
+            if res > eq[0]:
+                break
         if res == eq[0]:
-            tot += eq[0]
-            break
+            return eq[0]
+    return 0
+
+with multiprocessing.Pool(12) as pool:
+    tot = sum(pool.map(check_eqn, eqs))
 
 assert tot == 61561126043536
