514aecb793686fcdd7217edc613544dfced69632
Alexander Björling <alexanbj@axis.com>
2024-12-09 09:29:11 +0100

    2024/09
    Change-Id: Ib1b5b9ac83dc75803c7fd021f4a4bdfa9e2e8ac3

--- 2024/09/part1.py
+++ 2024/09/part1.py
@@ -1,8 +1,10 @@
-
 with open("input.txt", "r") as fp:
     data = list(map(int, fp.read().strip()))
 
-def expand(line):
+def parse(line):
+    """
+    Take the compact input list, and parse to file blocks, -1 means space.
+    """
     ret = []
     fid = 0
     for i in range(len(line)):
@@ -16,13 +18,14 @@ def expand(line):
     return ret
 
 def compress(line):
+    """
+    Compress the list by the rules of the exercise.
+    """
     out = line.copy()
-    import numpy as np
-    tot = len(np.where(np.array(line) != -1)[0])
+    tot = len([x for x in line if x != -1])
     for n in line[::-1]:
         if n == -1:
             continue
-
         # see where to put it
         i2 = out.index(-1)
         if i2 > tot:
@@ -31,9 +34,12 @@ def compress(line):
     return(out[:tot])
 
 def check(line):
+    """
+    Sum up the compressed disk.
+    """
     tot = 0
     for i, n in enumerate(line):
         tot += i * n
     return tot
 
-print(check(compress(expand(data))))
+assert check(compress(parse(data))) == 6288707484810
--- 2024/09/part2.py
+++ 2024/09/part2.py
@@ -16,7 +16,10 @@ class File():
     def __str__(self):
         return str(self.id) * self.width
 
-def expand(line):
+def parse(line):
+    """
+    Parse the input into a list of Space and File objects
+    """
     ret = []
     fid = 0
     for i in range(len(line)):
@@ -30,28 +33,56 @@ def expand(line):
     return ret
 
 def compress(inp):
+    """
+    Compress the list of Space and File objects by the rules of the exercise.
+    """
     N = len([f for f in inp if isinstance(f, File)])
     out = inp.copy()
-    for i1 in range(1, len(inp)):
-        f1 = inp[-i1]
-        if isinstance(f1, Space):
+
+    # loop over all files, starting on the right
+    for f1 in inp[::-1]:
+        if not isinstance(f1, File):
             continue
-        for i2 in range(len(out)):
+
+        # for the current state of the output list, where is f1?
+        i1 = [(isinstance(f_, File) and f_.id==f1.id) for f_ in out].index(True)
+
+        # is there space anywhere on the left?
+        for i2 in range(i1):
             f2 = out[i2]
             if isinstance(f2, Space) and f2.width >= f1.width:
+                # good, now replace f1 with space, and put f1 where the space was
                 out[i2] = f1
+                out[i1] = Space(f1.width)
+                # see if we should add more space to the right of f1, either by inserting
+                # a new Space or by increasing the size of one already there.
                 diff = f2.width - f1.width
                 if diff > 0:
-                    if isinstance(out[i2 + 1], Space):
-                        print('y')
-                        out[i2 + 1] = Space(diff + out[i2 + 1].width)
+                    if isinstance(out[i2 + 1], Space):  # never actually used, but could have been important
+                        out[i2 + 1] = Space(out[i2 + 1].width + diff)
                     else:
-                        print('n')
                         out.insert(i2 + 1, Space(diff))
                 break
     return out
 
+def expand(line):
+    """
+    Expand the files and spaces to a list of blocks, each with the id of its file (spaces are 0).
+    """
+    out = []
+    done = set()
+    for f in line:
+        if isinstance(f, File) and f.id not in done:
+            out += [f.id,] * f.width
+            done.add(f.id)
+        else:
+            out += [0,] * f.width
+    return out
+
 def check(line):
+    """
+    Sum up the compressed disk
+    """
     tot = 0
     for i, n in enumerate(line):
         if n == -1:
@@ -59,17 +90,6 @@ def check(line):
         tot += i * n
     return tot
 
-def expand2(line):
-    out = []
-    done = set()
-    print([str(c) for c in line])
-    for f in line:
-        if isinstance(f, File) and f.id not in done:
-            out += [f.id,] * f.width
-            done.add(f.id)
-        else:
-            out += [0,] * f.width
-    print([c for c in out])
-    return out
 
-print(check(expand2(compress(expand(data)))))
+
+assert check(expand(compress(parse(data)))) == 6311837662089
