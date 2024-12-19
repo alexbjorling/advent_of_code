a22ae11c3af8f52365b792cf05843979d75697a8
Alexander Björling <alexanbj@axis.com>
2024-12-19 09:56:37 +0100

    2024/19 tidying
    Change-Id: I31eda5546e7493ab142b70e8da1c90ca9a6175e4

--- 2024/19/part1.py
+++ 2024/19/part1.py
@@ -3,23 +3,21 @@ with open("input.txt", "r") as fp:
     towels = towels.split(", ")
     patterns = patterns.splitlines()
 
+# Find any solution, by returning 1 as soon as we reach the end of the string.
 def search(pattern):
     if len(pattern) == 0:
         return 1
 
-    tot = 0
+    solved = 0
     for towel in towels:
         n = len(towel)
         if pattern[:n] == towel:
-            tot += search(pattern[n:])
-            if tot:  # bail early if we find a solution
-                return tot
-
-    return tot
+            if search(pattern[n:]):
+                return 1
+    return solved
 
 tot = 0
 for p in patterns:
-    if search(p):
-        tot += 1
+    tot += search(p)
 
 assert tot == 216
--- 2024/19/part2.py
+++ 2024/19/part2.py
@@ -1,22 +1,36 @@
-from functools import lru_cache
-
 with open("input.txt", "r") as fp:
     towels, patterns = fp.read().split("\n\n")
     towels = towels.split(", ")
     patterns = patterns.splitlines()
 
-@lru_cache
+
+# Define a home made caching decorator for fun, would normally
+# use functools.lru_cache.
+def homemade_cache(func):
+    cache = {}
+    def call(arg):
+        try:
+            return cache[arg]
+        except KeyError:
+            cache[arg] = func(arg)
+            return cache[arg]
+    return call
+
+
+# Search through the whole tree of possible towel combinations,
+# adding up the number of solutions found.
+@homemade_cache
 def search(pattern):
     if len(pattern) == 0:
         return 1
 
-    tot = 0
+    solutions = 0
     for towel in towels:
         n = len(towel)
         if pattern[:n] == towel:
-            tot += search(pattern[n:])
+            solutions += search(pattern[n:])
+    return solutions
 
-    return tot
 
 tot = 0
 for p in patterns:
