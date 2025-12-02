b3bccccf70bf73378bd5f6c51f024e21b7135abf
Alexander Björling <alexanbj@axis.com>
2025-12-02 22:14:05 +0100

    2025/02, cleaned up

---
+++ 2025/02/part12.cc
@@ -0,0 +1,105 @@
+#include <algorithm> // find
+#include <cassert>
+#include <fstream>
+#include <stdexcept>
+#include <string>
+#include <string_view>
+#include <vector>
+
+// helper to split a string on a char
+std::vector<std::string> split(const std::string& str, const char delimiter) {
+    std::vector<std::string> res;
+    auto p2 = str.begin();
+    while (p2 != str.end()) {
+        auto p1 = p2;
+        p2 = std::find(p1, str.end(), delimiter);
+        auto sv = std::string(p1, p2);
+        res.push_back(sv);
+        if (p2 != str.end()) {
+            p2++;
+        }
+    }
+    return res;
+}
+
+// parse data using split
+std::vector<std::pair<long, long>> parse(const std::string& strdata) {
+    std::vector<std::pair<long, long>> res;
+    for (auto& substr : split(strdata, ',')) {
+        auto pair = split(substr, '-');
+        res.push_back({std::stol(pair[0]), std::stol(pair[1])});
+    }
+    return res;
+}
+
+// check if a number is made up of n identical sub-numbers
+bool is_repeat(long num, int n) {
+    // make a string and check that it can be split into n_repeat parts
+    std::string s = std::to_string(num);
+    if (s.size() % n != 0) {
+        return false;
+    }
+
+    // loop through all the n_repeat parts, checking for identity
+    int len = s.size() / n;
+    std::string last(s.begin(), s.begin() + len);
+    for (size_t i = 1; i < n; i++) {
+        std::string current(s.begin() + i * len, s.begin() + (i + 1) * len);
+        if (current != last) {
+            return false;
+        }
+        last = current;
+    }
+    return true;
+}
+
+// check if a number is the repetition of a sub-number, at least twice
+bool is_repeat(long num) {
+    std::string s = std::to_string(num);
+    for (int splits = 2; splits <= s.size(); splits++) {
+        if (is_repeat(num, splits)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    // read data
+    std::ifstream ifs("input.txt");
+    if (ifs.fail()) {
+        throw std::runtime_error("Failed to open input file");
+    }
+    std::string strdata;
+    std::getline(ifs, strdata);
+
+    // split the string into <long, long> pairs
+    auto ranges = parse(strdata);
+
+    // loop over the ranges, checking through each one, part 1
+    long total = 0;
+    for (auto& rng : ranges) {
+        int x = 0;
+        for (long i = rng.first; i <= rng.second; i++) {
+            if (is_repeat(i, 2)) {
+                total += i;
+            }
+        }
+    }
+    assert(total == 26255179562);
+
+    // loop over the ranges, checking through each one, part 2
+    total = 0;
+    for (auto& rng : ranges) {
+        int x = 0;
+        for (long i = rng.first; i <= rng.second; i++) {
+            //if (is_pair(i)) { // part 1
+            if (is_repeat(i)) { // part 2
+                total += i;
+            }
+        }
+    }
+    assert(total == 31680313976);
+
+    return 0;
+}
