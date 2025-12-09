d3e29fd19ae12ab6be204360ad6bcf5254a59f20
Alexander Björling <alexanbj@axis.com>
2025-12-09 14:02:18 +0100

    2025/09 pt 2 algebra attempt
    Change-Id: Ib7289d6ed1d43c258ecd10d1b77fbca920f49ddb

--- 2025/09/lines.hh
+++ 2025/09/lines.hh
@@ -1,73 +1,70 @@
 #include <array>
 #include <cassert>
+#include <iostream>
 
 // helper class which holds a line segment, and can check whether it crosses
 // another line segment.
-class Line {
-public:
+struct Line {
     Line(std::pair<int, int> p1, std::pair<int, int> p2)
     : x0(p1.first)
     , y0(p1.second)
     , x1(p2.first)
     , y1(p2.second)
     {}
-
-    // use the + operator to return whether the lines cross or not,
-    // with one line ending on the other is not considered a cross
-    bool operator+(Line& other) {
-        // parametrize the lines a and b,
-        // with a0 = (x0, y0), a1 = (x1, xy) for line as, and vice versa
-        // then set
-        // a0 + ta * (a1 - a0) = b0 + tb * (b1 - b0) and solve for (ta, tb)
-
-        // A = t * b, where
-        const std::array<std::array<int, 2>, 2> A {{
-            {x1 - x0, other.x1 - other.x0},
-            {y1 - y0, other.y1 - other.y0}
-        }};
-        const std::array<std::array<int, 2>, 2> B {{
-            {1, 2},
-            {3, 4}
-        }};
-        // and
-        std::array<int, 2> b {other.x0 - x0, other.y0 - y0};
-
-        // get and check the determinant
-        int det = A[0][0] * A[1][1] - A[0][1] * A[1][0];
-        if (det == 0) {
-            // lines are parallel
-            return 0;
-        }
-
-        // find the inverse matrix
-        double d = 1.0 / det;
-        const std::array<std::array<double, 2>, 2> Ainv {{
-            {A[1][1] * d, -A[0][1] * d},
-            {-A[1][0] * d, A[0][0] * d}
-        }};
-
-        // solve for the parameters and check for crossing
-        double ta = b[0] * Ainv[0][0] + b[1] * Ainv[0][1];
-        double tb = -(b[0] * Ainv[1][0] + b[1] * Ainv[1][1]);
-        return (ta > 0 and ta < 1 and tb > 0 and tb < 1);
-    }
-
-    // equality operator
-    bool operator==(Line& other) {
-        return (x0 == other.x0 and y0 == other.y0 and x0 == other.x0 and x1 == other.x1);
-    }
-private:
     int x0, y0, x1, y1;
 };
 
-bool check_lines(Line l1, Line l2) {
-        return l1 + l2;
+bool line_cross(Line l1, Line l2) {
+    bool l1_horizontal = (l1.y0 == l1.y1);
+    bool l2_horizontal = (l2.y0 == l2.y1);
+    if ((l1_horizontal and l2_horizontal) or (!l1_horizontal and !l2_horizontal)) {
+        // if the lines are parallel they won't cross
+        return false;
+    }
+    if (l1_horizontal) {
+        // l1 is horizontal, l2 is vertical
+        return (l1.x0 < l2.x0 and l1.x1 > l2.x0) and (l2.y0 < l1.y0 and l2.y1 > l1.y0);
+    } else {
+        // l1 is vertical, l2 is horizontal
+        return (l2.x0 < l1.x0 and l2.x1 > l1.x0) and (l1.y0 < l2.y0 and l1.y1 > l2.y0);
+    }
+}
+
+// does the line l cross the intersection of the joined line segments p1 and p2?
+bool node_cross(Line p1, Line p2, Line l) {
+    assert (p1.x1 == p2.x0 and p1.y1 == p2.y0); // establish the order
+    // check if the node is on the line segment
+    const int nodex = p1.x1;
+    const int nodey = p1.y1;
+    if ((nodex < std::min(l.x0, l.x1) or nodex > std::max(l.x0, l.x1))) {
+        // the node is outside the x of the line segment
+        return false;
+    }
+    if (nodey < std::min(l.y0, l.y1) or nodey > std::max(l.y0, l.y1)) {
+        // the node is outside the y range of the line segment
+        return false;
+    }
+    if ((nodex - l.x0) * (l.y1 - l.y0) - (nodey - l.y0) * (l.x1 - l.x0) != 0) {
+        // this means the node does not lie on the line by the determinant
+        return false;
     }
 
-void test_algebra() {
-    assert (true == check_lines({{0,0},{5,5}}, {{0,2},{5,2}}));
-    assert (false == check_lines({{0,0},{5,5}}, {{1,1},{5,1}})); // false (meet but don't cross)
-    assert (false == check_lines({{0,0},{5,5}}, {{2,1},{5,1}})); // false
-    assert (false == check_lines({{0,0},{5,0}}, {{0,2},{5,2}})); // false, parallel
-    assert (true == check_lines({{0,0},{5,0}}, {{2,-2},{2,2}})); // true
+    // check with cross products whether the line crosses the path or not
+    float cross1 = (l.x1 - l.x0) * (p1.y1 - p1.y0) - (p1.x1 - p1.x0) * (l.y1 - l.y0);
+    float cross2 = (l.x1 - l.x0) * (p2.y1 - p2.y0) - (p2.x1 - p2.x0) * (l.y1 - l.y0);
+    return ((cross1 > 0 and cross2 > 0) or (cross1 < 0 and cross2 < 0));
+
+}
+
+void test_lines_helper() {
+    // trivial maths since lines are horizontal or vertical, check line_cross
+    assert (false == line_cross({{0,2},{5,2}}, {{5,2},{10,2}})); // end to end
+    assert (false == line_cross({{0,2},{5,2}}, {{4,2},{4,5}})); // end to side
+    assert (false == line_cross({{0,2},{5,2}}, {{4,2},{7,2}})); // parallel and coincident
+    assert (true == line_cross({{0,2},{5,2}}, {{4,0},{4,7}})); // cross one way
+    assert (true == line_cross({{2,-2},{2,2}}, {{1,1},{3,1}})); // cross the other way
+
+    // check node_cross
+    assert (true == node_cross({{0, 0},{4, 4}}, {{4, 4},{6, 5}}, {{4, 0},{4, 8}}));
+    assert (false == node_cross({{0, 0},{4, 4}}, {{4, 4},{3, 6}}, {{4, 0},{4, 8}}));
 }
--- 2025/09/part2.cc
+++ 2025/09/part2.cc
@@ -37,7 +37,53 @@ std::vector<Tile> load(const std::string& fn) {
 }
 
 int main() {
-    auto tiles = load("input.txt");
-    test_algebra();
+    test_lines_helper();
+    auto tiles = load("ex.txt");
+
+    // assemble a polygon to check agains
+    std::vector<Line> polygon;
+    for (size_t i = 1; i < tiles.size(); i++) {
+        polygon.push_back(Line({tiles[i-1].x, tiles[i-1].y}, {tiles[i].x, tiles[i].y}));
+    }
+    polygon.push_back(Line({tiles.back().x, tiles.back().y}, {tiles[0].x, tiles[0].y}));
+
+    // loop through all the
+    std::vector<long> areas;
+    for (size_t i = 0; i < tiles.size(); i++) {
+        for (size_t j = 0; j < i; j++) {
+            // the edges of the current rectangle
+            Line l1 ({tiles[i].x, tiles[i].y}, {tiles[i].x, tiles[j].y});
+            Line l2 ({tiles[i].x, tiles[j].y}, {tiles[j].x, tiles[j].y});
+            Line l3 ({tiles[j].x, tiles[j].y}, {tiles[j].x, tiles[i].y});
+            Line l4 ({tiles[i].x, tiles[i].y}, {tiles[j].x, tiles[i].y});
+
+            // is this a valid rectangle?
+            bool skip = false;
+            for (size_t k = 0; k < polygon.size(); k++) {
+                // do any of the rectangle edges intersect this polygon edge?
+                const Line& p = polygon[k];
+                if (line_cross(p, l1) or line_cross(p, l2) or line_cross(p, l3) or line_cross(p, l4)) {
+                    skip = true;
+                    break;
+                }
+
+                // do any of the rectangle edges cross the polygon at one of its corners?
+                const Line& p1 = (k == polygon.size() - 1) ? polygon[0] : polygon[k+1];
+                if (node_cross(p, p1, l1) or node_cross(p, p1, l2) or node_cross(p, p1, l3) or node_cross(p, p1, l4)) {
+                    skip = true;
+                    break;
+                }
+            }
+            if (skip) { continue; }
+            // if yes, add the candidate area
+            areas.push_back(tiles[j] - tiles[i]);
+        }
+    }
+    long ans = *std::max_element(areas.cbegin(), areas.cend());
+    printf("** %ld\n", ans);
+    // line_cross fixes the example but gives the p1 answer for the input
+    // 3197574321 high
+    // 4555903716
+    // 4740155680 part 1
     return 0;
 }
