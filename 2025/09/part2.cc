82a3cd2351cb7e2b82a3ddc79fea87e1acfa7db8
Alexander Björling <alexanbj@axis.com>
2025-12-09 18:58:45 +0100

    2025/09 pt2 attempt with PIP on a grid -- too slow
    Change-Id: Iac54fa730a6c5f07b2c413044c6b7583654dad9a

--- 2025/09/part2.cc
+++ 2025/09/part2.cc
@@ -1,4 +1,4 @@
-#include "lines.hh"
+#include "part2_utils.hh"
 
 #include <cassert>
 #include <fstream>
@@ -7,28 +7,19 @@
 #include <string>
 #include <vector>
 
-// helper class to store positions and take distances
-struct Tile {
-    long x, y;
-    // tile1 - tile2 gives the area spanned by the two tiles
-    long operator-(const Tile& other) const {
-        return std::abs((x - other.x + 1) * (y - other.y + 1));
-    }
-};
-
 // open the input file and make a vector of boxes
-std::vector<Tile> load(const std::string& fn) {
+std::vector<Point> load(const std::string& fn) {
     std::ifstream ifs(fn);
     if (ifs.fail()) {
         throw std::runtime_error("Failed to open input file");
     }
-    std::vector<Tile> tiles {};
+    std::vector<Point> tiles {};
     std::string line;
     while (std::getline(ifs, line)) {
         std::smatch match;
         std::regex pattern {"([0-9]+),([0-9]+)"};
         std::regex_match(line, match, pattern);
-        Tile tile {};
+        Point tile {};
         tile.x = std::stoi(match[1].str());
         tile.y = std::stoi(match[2].str());
         tiles.push_back(tile);
@@ -37,50 +28,58 @@ std::vector<Tile> load(const std::string& fn) {
 }
 
 int main() {
-    test_lines_helper();
-    auto tiles = load("ex.txt");
+    test_geometry();
+    auto tiles = load("input.txt");
 
-    // assemble a polygon to check agains
+    // assemble a polygon to check against
     std::vector<Line> polygon;
     for (size_t i = 1; i < tiles.size(); i++) {
         polygon.push_back(Line({tiles[i-1].x, tiles[i-1].y}, {tiles[i].x, tiles[i].y}));
     }
     polygon.push_back(Line({tiles.back().x, tiles.back().y}, {tiles[0].x, tiles[0].y}));
 
-    // loop through all the
-    std::vector<long> areas;
+    // loop through all the tile pairs, calculating the area and
+    // sorting the options. this is quick.
+    typedef std::pair<long, std::pair<size_t, size_t>> option;
+    std::vector<option> options;
     for (size_t i = 0; i < tiles.size(); i++) {
         for (size_t j = 0; j < i; j++) {
-            // the edges of the current rectangle
-            Line l1 ({tiles[i].x, tiles[i].y}, {tiles[i].x, tiles[j].y});
-            Line l2 ({tiles[i].x, tiles[j].y}, {tiles[j].x, tiles[j].y});
-            Line l3 ({tiles[j].x, tiles[j].y}, {tiles[j].x, tiles[i].y});
-            Line l4 ({tiles[i].x, tiles[i].y}, {tiles[j].x, tiles[i].y});
-
-            // is this a valid rectangle?
-            bool skip = false;
-            for (size_t k = 0; k < polygon.size(); k++) {
-                // do any of the rectangle edges intersect this polygon edge?
-                const Line& p = polygon[k];
-                if (line_cross(p, l1) or line_cross(p, l2) or line_cross(p, l3) or line_cross(p, l4)) {
-                    skip = true;
-                    break;
-                }
-
-                // do any of the rectangle edges cross the polygon at one of its corners?
-                const Line& p1 = (k == polygon.size() - 1) ? polygon[0] : polygon[k+1];
-                if (node_cross(p, p1, l1) or node_cross(p, p1, l2) or node_cross(p, p1, l3) or node_cross(p, p1, l4)) {
-                    skip = true;
-                    break;
-                }
-            }
-            if (skip) { continue; }
-            // if yes, add the candidate area
-            areas.push_back(tiles[j] - tiles[i]);
+            options.push_back({tile_area(tiles[i], tiles[j]), {i, j}});
         }
     }
-    long ans = *std::max_element(areas.cbegin(), areas.cend());
-    printf("** %ld\n", ans);
+    std::sort(options.begin(), options.end(), [](option& op1, option& op2){ return op1.first > op2.first; });
+
+    // find a maximum x value for the PIP algorithm later
+    std::vector<int> x {};
+    for (auto& p : tiles) {
+        x.push_back(p.x);
+    }
+    const int xmax = *std::max_element(x.cbegin(), x.cend());
+
+    // loop through the options until we find an acceptable one
+    long area = 0;
+    for (auto& opt : options) {
+        // find all the tiles on the rectangle border
+        auto& t1 = tiles[opt.second.first];
+        auto& t2 = tiles[opt.second.second];
+        auto border = render_tile_border(t1, t2);
+
+        // check if each lies on one of the polygon edges
+        bool done = true;
+        for (auto& b : border) {
+            if (point_outside_polygon(b, polygon, xmax)) {
+                done = false;
+                break;
+            }
+        }
+
+        if (done) {
+            area = opt.first;
+            break;
+        }
+    }
+
+    printf("area = %ld\n", area);
     // line_cross fixes the example but gives the p1 answer for the input
     // 3197574321 high
     // 4555903716
